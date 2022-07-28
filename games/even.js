e7586439626eb0862d781188eb3c74d313e55216
murat72 <muratkunanbaev1991@gmail.com>
2022-07-28 23:09:53 +0500

    fixed bug

--- games/even.js
+++ games/even.js
@@ -1,24 +1,18 @@
-import {numberAttempts, showGreeating, compareUserAnswer, askQuestion} from '../src/index.js';
-
-const max = 100;
-
-const randomNumber = () => {
-  return Math.floor((Math.random() * max) + 1);
-}
+import {numberAttempts, showGreeating, compareUserAnswer, askQuestion, generateRandomNumber} from '../src/index.js';
 
 const evenNumber = () => {
   let countUserAttempts = 1;
   const userName = showGreeating();
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   while(countUserAttempts <= numberAttempts) {
-    const number = randomNumber();
+    const number = generateRandomNumber(1, 100);
     const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
     const userAnswer = askQuestion(number).toLowerCase();
     if (compareUserAnswer(userAnswer, correctAnswer, userName, countUserAttempts)) {
       countUserAttempts += 1;
-  } else {
+    } else {
       break;
-  }
+    }
   }
 };
 
