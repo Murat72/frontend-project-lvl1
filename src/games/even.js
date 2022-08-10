b8242742ac6eba6f2190fabe7f91bfbf0f408013
murat72 <muratkunanbaev1991@gmail.com>
2022-08-10 18:46:03 +0500

    fixed bugs

--- src/games/even.js
+++ src/games/even.js
@@ -3,7 +3,7 @@ import generateRandomNumber from '../utils.js';
 
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven  = (number) => (number % 2 === 0 ? true : false);
+const isEven = (number) => {return number % 2 === 0 };
 
 const generateRound = () => {
   const number = generateRandomNumber(1, 100);
